ac799e3d02136c33f3e5b4feb4ccdd53e821ce97
tuongvyluan <luantuongvy13@gmail.com>
2022-06-01 11:39:34 +0700

    Student.java

---
+++ Hehe/src/main/java/dto/Student.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package dto;
+
+/**
+ * @author Tuong Vy Luan
+ *
+ */
+public class Student {
+    private String id;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+	return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+	this.id = id;
+    }
+}
